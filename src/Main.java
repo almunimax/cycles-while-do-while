a8cd3dc383e45acec78a44244573cecfe10abbdd
almunimax <almunimax@mail.ru>
2022-12-09 15:21:03 +0300

    дз 1-3

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,37 @@ public class Main {
         int month = 0;
         while (total < 2_459_000) {
             total = total + salary;
-            month = month + 1;
+            month++;
             System.out.println("Месяц " + month + ",сумма накоплений равна " + total + " рублей");
-                    }
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int z = 10; z > 0; z--) {
+            System.out.print(z + " ");
+        }
+        System.out.println();
+
+
+        // Задание 3
+        System.out.println("Задание 3");
+
+        int population = 12000000;
+        int born = 17;
+        int death = 8;
+        int growth = born - death;
+        for(int year = 1; year <= 10; year++) {
+            population += population * growth / 1000;
+            System.out.println("Год " + year + " численноссть населения составляет " + population);
+        }
+
 
     }
 }
